ada8e745341553d8de43455401bc5a9e8855839c
Jayanth reddy <devireddyjayanth@gmail.com>
2021-07-23 19:50:41 +0530

    modified successfully

--- src/main/java/in/ashokit/MyAppDemo.java
+++ src/main/java/in/ashokit/MyAppDemo.java
@@ -3,12 +3,16 @@ package in.ashokit;
 public class MyAppDemo {
 
 	public static void main(String[] args) {
+		System.out.println("this is delip");
 		System.out.println("welcome to my Application....");
 		int i=20;
 		int j=30;
 		int k=i+j;
 		System.out.println(k);
-
 	}
+	public void delip(){
+       	
+          Syste.out.println("delip method....");
+       }
 
 }
