70aa462f3da06c18cef3a4ec20372b51ad60eb34
Jayanth reddy <devireddyjayanth@gmail.com>
2021-07-23 18:37:08 +0530

    resent modification in the file

--- src/main/java/in/ashokit/MyAppDemo.java
+++ src/main/java/in/ashokit/MyAppDemo.java
@@ -5,6 +5,9 @@ public class MyAppDemo {
 	public static void main(String[] args) {
 		System.out.println("welcome to my Application....");
 		int i=20;
+		int j=30;
+		int k=i+j;
+		System.out.println(k);
 
 	}
 
